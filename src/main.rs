8ebec3bd85afad3184768bc039e938104d48156b
rfoxmich <fox@frib.msu.edu>
2022-12-23 13:26:54 -0500

    *    Added new method to create RingItem objects. *    Added getters for the simple stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod ring_items;
 
 fn main() {
-    let _item = ring_items::RingItem {
-        size: 0,
-        type_id: 0,
-        body_header_size: 0,
-        payload: Vec::new(),
-    };
-    println!("Hello, world!");
+    let item = ring_items::RingItem::new(1);
+    println!("Size: {}", item.size());
+    println!("Type: {}", item.type_id());
+    println!("Has body header: {}", item.has_body_header());
 }
--- src/ring_items/mod.rs
+++ src/ring_items/mod.rs
@@ -1,11 +1,37 @@
-// This is an raw ring item.   Raw in the
-// sense that the payload is just a soup of bytes.
-// However it wil have methods that allow conversion of this item
-// to more structured ring items based on the 'type' field.
-//
+use std::mem;
+/// This is an raw ring item.   Raw in the
+/// sense that the payload is just a soup of bytes.
+/// However it wil have methods that allow conversion of this item
+/// to more structured ring items based on the 'type' field.
+///
 pub struct RingItem {
-    pub size: u32,
-    pub type_id: u32,
-    pub body_header_size: u32,
-    pub payload: Vec<u8>,
+    size: u32,
+    type_id: u32,
+    body_header_size: u32,
+    payload: Vec<u8>,
+}
+
+impl RingItem {
+    ///
+    /// Create a new empty ring item of the given type.
+    ///
+    pub fn new(t: u32) -> RingItem {
+        RingItem {
+            size: 3 * mem::size_of::<u32>() as u32,
+            type_id: t,
+            body_header_size: mem::size_of::<u32>() as u32,
+            payload: Vec::new(),
+        }
+    }
+    // getters:
+
+    pub fn size(&self) -> u32 {
+        self.size
+    }
+    pub fn type_id(&self) -> u32 {
+        self.type_id
+    }
+    pub fn has_body_header(&self) -> bool {
+        self.body_header_size > mem::size_of::<u32>() as u32
+    }
 }
