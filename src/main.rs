16c0a5c5c60a5c6c7b6450743f184b8dcaa1e065
rfoxmich <fox@frib.msu.edu>
2022-12-23 17:03:33 -0500

    Check that new_with_body_header constructs correctly for now

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ mod ring_items;
 
 fn main() {
     let item = ring_items::RingItem::new(1);
+    let item2 = ring_items::RingItem::new_with_body_header(
+        2, 0x123456789, 2, 0
+    );
+
     println!("Size: {}", item.size());
     println!("Type: {}", item.type_id());
     println!("Has body header: {}", item.has_body_header());
+
+    println!("Size: {}", item2.size());
+    println!("Type: {}", item2.type_id());
+    println!("Has body header:{}", item2.has_body_header());
+
 }
--- src/ring_items/mod.rs
+++ src/ring_items/mod.rs
@@ -23,6 +23,16 @@ impl RingItem {
             payload: Vec::new(),
         }
     }
+    pub fn new_with_body_header(t: u32, stamp: u64, source: u32, barrier: u32) -> RingItem {
+        let mut result = RingItem::new(t);
+        result.body_header_size = (3 * mem::size_of::<u32>() + mem::size_of::<u64>()) as u32;
+
+        result.add(stamp);
+        result.add(source);
+        result.add(barrier);
+
+        result
+    }
     // getters:
 
     pub fn size(&self) -> u32 {
@@ -33,5 +43,21 @@ impl RingItem {
     }
     pub fn has_body_header(&self) -> bool {
         self.body_header_size > mem::size_of::<u32>() as u32
+    }
+    pub fn add<T>(&mut self, item: T) -> &mut RingItem {
+        let pt = &item as *const T;
+        let mut p = pt.cast::<u8>();
+
+        // Now I have a byte pointer I can push the bytes of data
+        // into the vector payload:
+
+        for _i in 0..mem::size_of::<T>() {
+            unsafe {
+                self.payload.push(*p);
+                p = p.offset(1);
+            }
+        }
+        self.size = self.size + mem::size_of::<T>() as u32;
+        self
     }
 }
