1d2cadf26390a3f2c3a7eaa4a3bdc1e9e50e5287
rfoxmich <fox@frib.msu.edu>
2023-01-01 15:43:33 -0500

    Start implementing text_item support

---
+++ src/ring_items/text_item.rs
@@ -0,0 +1,159 @@
+use crate::ring_items;
+use std::mem;
+use std::time;
+///
+///  This module provides support for ring items that have
+/// payloads that consist of textual strings.  This ring items
+/// are normally used for documentation purposes
+///
+
+/// More than one type of raw item may be a text item.  This
+///
+#[derive(Clone, Copy)]
+pub enum TextItemType {
+    PacketTypes,
+    MonitoredVariables,
+}
+
+/// the TextItem struct is the internal representation of a textual
+/// item.  The Option struct members are those for which, depending
+/// on the actual manner of construction of the item (body header or no)
+/// or depending on the NSCLDAQ version (V11/V12) may or may not be present
+/// in the raw ring item.
+
+pub struct TextItem {
+    item_type: TextItemType,
+    body_header: Option<ring_items::BodyHeader>,
+    time_offset: u32,
+    absolute_time: time::SystemTime,
+    offset_divisor: u32,
+    original_sid: Option<u32>,
+    strings: Vec<String>,
+}
+
+impl TextItem {
+    fn item_type_to_int(item_type: TextItemType) -> u32 {
+        match item_type {
+            TextItemType::PacketTypes => ring_items::PACKET_TYPES,
+            TextItemType::MonitoredVariables => ring_items::MONITORED_VARIABLES,
+        }
+    }
+    fn string_from_item_type(item_type: TextItemType, vers: ring_items::RingVersion) -> String {
+        match item_type {
+            TextItemType::PacketTypes => String::from("Packet Types"),
+            TextItemType::MonitoredVariables => String::from("Monitored variables"),
+        }
+    }
+    fn item_type_from_u32(type_id: u32) -> Option<TextItemType> {
+        match type_id {
+            ring_items::PACKET_TYPES => Some(TextItemType::PacketTypes),
+            ring_items::MONITORED_VARIABLES => Some(TextItemType::MonitoredVariables),
+            _ => None,
+        }
+    }
+    /// Create a new item:
+
+    pub fn new(
+        type_id: TextItemType,
+        body_header: Option<ring_items::BodyHeader>,
+        offset: u32,
+        timestamp: time::SystemTime,
+        divisor: u32,
+        orsid: Option<u32>,
+        strings: &Vec<String>,
+    ) -> TextItem {
+        TextItem {
+            item_type: type_id,
+            body_header: body_header,
+            time_offset: offset,
+            absolute_time: timestamp,
+            offset_divisor: divisor,
+            original_sid: orsid,
+            strings: strings.clone(),
+        }
+    }
+    // Getters.
+
+    pub fn get_item_type(&self) -> TextItemType {
+        self.item_type
+    }
+    pub fn get_body_header(&self) -> Option<ring_items::BodyHeader> {
+        self.body_header
+    }
+    pub fn get_time_offset(&self) -> u32 {
+        self.time_offset
+    }
+    pub fn get_offset_secs(&self) -> f32 {
+        (self.time_offset as f32) / (self.offset_divisor as f32)
+    }
+    pub fn get_original_sid(&self) -> Option<u32> {
+        self.original_sid
+    }
+    pub fn get_strings(&self) -> Vec<String> {
+        self.strings.clone()
+    }
+    pub fn get_string(&self, index: usize) -> Option<String> {
+        if index < self.strings.len() {
+            Some(self.strings[index].clone())
+        } else {
+            None
+        }
+    }
+
+    // Conversions.
+
+    pub fn from_raw(raw: &ring_items::RingItem, vers : ring_items::RingVersion) -> Option<TextItem> {
+        // figure out the correct value for the
+        // type:
+
+        if let Some(itype) = Self::item_type_from_u32(raw.type_id()) {
+            let mut result = TextItem {
+                item_type: itype,
+                body_header: raw.get_bodyheader(),
+                time_offset: 0,
+                absolute_time: time::SystemTime::now(),
+                offset_divisor: 1,
+                original_sid: None,
+                strings: Vec::new(),
+            };
+            // Now fill in the rest of the result with stuff
+            // from the raw item.  The offset of the payload
+            //  depends on the existence
+            // or nonexistence of a body header
+
+            let offset: usize = if result.body_header.is_some() {
+                ring_items::body_header_size() as usize
+            } else {
+                0
+            };
+            let p = raw.payload().as_slice();
+            result.time_offset = u32::from_ne_bytes(p[offset..offset + 4].try_into().unwrap());
+            result.absolute_time = ring_items::raw_to_systime(u32::from_ne_bytes(
+                p[offset + 4..offset + 8].try_into().unwrap(),
+            ));
+            let num_string = u32::from_ne_bytes(p[offset + 8..offset + 12].try_into().unwrap());
+            result.offset_divisor =
+                u32::from_ne_bytes(p[offset + 12..offset + 16].try_into().unwrap());
+            let mut offset = offset + 16;
+            if vers == ring_items::RingVersion::V12 {
+                result.original_sid = Some(u32::from_ne_bytes(
+                    p[offset..offset + 4].try_into().unwrap(),
+                ));
+                offset = offset + 4;
+            }
+            // offset is the offset of the first string.
+
+            for _ in 0..num_string {
+                let slen = ring_items::string_len(&p[offset..]);
+                result
+                    .strings
+                    .push(String::from_utf8(p[offset..offset + slen].try_into().unwrap()).unwrap());
+                offset = offset + slen;
+            }
+
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
