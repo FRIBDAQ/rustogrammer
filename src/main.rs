482d38975c213af2149fac20c32d32cb4ccb040c
rfoxmich <fox@frib.msu.edu>
2023-01-10 13:36:22 -0500

    *   Dump abnormal end items. *   Use the analysis_ring_items module.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ring_items;
 use humantime;
+use ring_items::abnormal_end;
 use ring_items::event_item;
 use ring_items::format_item;
 use ring_items::glom_parameters;
@@ -10,7 +11,6 @@ use ring_items::triggers_item;
 use std::fs::File;
 
 fn main() {
-
     if let Ok(mut f) = File::open("run-0088-00.evt") {
         dump_items(&mut f);
     } else {
@@ -81,6 +81,11 @@ fn dump_items(f: &mut File) {
                 dump_glom_parameters(&gp);
                 let raw = gp.to_raw();
                 println!("Recreate size: {} type:{}", raw.size(), raw.type_id());
+            }
+            if let Some(ae) = abnormal_end::AbnormalEnd::from_raw(&item) {
+                println!("Abnormal end");
+                let raw = ae.to_raw();
+                println!("Recreate size: {} type {}", raw.size(), raw.type_id());
             }
         } else {
             println!("done");
@@ -208,9 +213,12 @@ fn dump_count_item(c: &triggers_item::PhysicsEventCountItem) {
         println!("Original sid: {}", sid);
     }
 }
-fn dump_glom_parameters(gp : &glom_parameters::GlomParameters) {
+fn dump_glom_parameters(gp: &glom_parameters::GlomParameters) {
     println!("Glom Parameters item");
-    println!("Coincidence interval {}, building? {}, ts policy {}",
-        gp.get_coincidence_interval(), gp.is_building(), gp.policy_string()
+    println!(
+        "Coincidence interval {}, building? {}, ts policy {}",
+        gp.get_coincidence_interval(),
+        gp.is_building(),
+        gp.policy_string()
     );
 }
