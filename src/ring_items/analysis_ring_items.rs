ab827aa557de38d16d8ac2e894ef5a801af31aaa
rfoxmich <fox@frib.msu.edu>
2023-01-11 12:05:24 -0500

    Added support for VariableValues ring items.

--- src/ring_items/analysis_ring_items.rs
+++ src/ring_items/analysis_ring_items.rs
@@ -64,7 +64,7 @@ impl ParameterDefinitions {
     ///
     pub fn to_raw(&self) -> ring_items::RingItem {
         let mut result = ring_items::RingItem::new(ring_items::PARAMETER_DEFINITIONS);
-        result.add(self.defs.len());
+        result.add(self.defs.len() as u32);
         for def in &self.defs {
             result.add(def.id);
             let mut bytes = String::into_bytes(def.name.clone());
@@ -91,5 +91,99 @@ impl ParameterDefinitions {
         *offset = offset.checked_add(4).unwrap();
         let name = ring_items::get_c_string(offset, &bytes);
         ParameterDefinition::new(id, &name)
+    }
+}
+//-------------------------------------------------------------
+// Variable values.
+//-------------------------------------------------------------
+const MAX_UNITS_LENGTH: usize = 32;
+///
+/// Each variable has a record that describes its value, name and units:
+///
+pub struct VariableValue {
+    value: f64,
+    name: String,
+    units: String,
+}
+
+impl VariableValue {
+    pub fn new(value: f64, name: &str, units: &str) -> VariableValue {
+        VariableValue {
+            value: value,
+            name: String::from(name),
+            units: String::from(units),
+        }
+    }
+
+    pub fn value(&self) -> f64 {
+        self.value
+    }
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+    pub fn units(&self) -> String {
+        self.units.clone()
+    }
+}
+
+///
+/// The variable item is really just a sequence of variable values:
+///
+pub struct VariableValues {
+    defs: Vec<VariableValue>,
+}
+impl VariableValues {
+    pub fn new() -> VariableValues {
+        VariableValues {
+            defs: Vec::<VariableValue>::new(),
+        }
+    }
+    pub fn iter(&self) -> Iter<'_, VariableValue> {
+        self.defs.iter()
+    }
+    /// Convert from raw if possible:
+
+    pub fn from_raw(raw: &ring_items::RingItem) -> Option<VariableValues> {
+        if raw.type_id() == ring_items::VARIABLE_VALUES {
+            let mut result = Self::new();
+            let payload = raw.payload().as_slice();
+            let nvars = u32::from_ne_bytes(payload[0..4].try_into().unwrap());
+            let mut offset = 4;
+            for _ in 0..nvars {
+                let value: f64 =
+                    f64::from_ne_bytes(payload[offset..offset + 8].try_into().unwrap());
+                let mut off = offset + 8;
+                let units = ring_items::get_c_string(&mut off, &payload);
+                offset = offset + MAX_UNITS_LENGTH;
+                let name = ring_items::get_c_string(&mut offset, &payload);
+                result.defs.push(VariableValue::new(value, &name, &units));
+            }
+            Some(result)
+        } else {
+            None
+        }
+    }
+    /// Convert to a raw ring item.
+
+    pub fn to_raw(&self) -> ring_items::RingItem {
+        // These never have a body  header:
+
+        let mut result = ring_items::RingItem::new(ring_items::VARIABLE_VALUES);
+        result.add(self.defs.len() as u32);
+        for def in self.defs.iter() {
+            result.add(def.value);
+            // build a units string padded with nulls out to MAX_UNITS_LENGTH:
+
+            let mut units_bytes = String::into_bytes(def.units.clone());
+            while units_bytes.len() < MAX_UNITS_LENGTH {
+                units_bytes.push(0);
+            }
+            result.add_byte_vec(&units_bytes);
+
+            let mut title_bytes = String::into_bytes(def.name.clone());
+            title_bytes.push(0);
+            result.add_byte_vec(&title_bytes);
+        }
+        result
     }
 }
