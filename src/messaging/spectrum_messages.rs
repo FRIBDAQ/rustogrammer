d41f058a046ca4b92ae73c08445cafe458b4beb0
rfoxmich <fox@frib.msu.edu>
2023-03-22 14:37:44 -0400

    Coded most of the SpectrumProcessor methods but need to add some mechanics to Spectrum trait to support List operation

--- src/messaging/spectrum_messages.rs
+++ src/messaging/spectrum_messages.rs
@@ -12,7 +12,11 @@
 use super::*;
 use crate::conditions;
 use crate::parameters;
-use crate::spectra::*;
+use crate::spectra;
+
+use glob::Pattern;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct AxisSpecification {
@@ -124,15 +128,310 @@ pub enum SpectrumReply {
 /// Note that the implementation is divorced from the
 /// actual message.  This makes testing the impl easier.
 pub struct SpectrumProcessor {
-    dict: SpectrumDictionary,
+    dict: spectra::SpectrumDictionary,
 }
 
+type ParamLookupResult = Result<u32, String>;
+type ParamsLookupResult = Result<Vec<u32>, String>;
 impl SpectrumProcessor {
+    // private methods:
+
+    // Make a 1-d spectrum:
+
+    fn make_1d(
+        &mut self,
+        name: &str,
+        parameter: &str,
+        axis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        let sname = String::from(name);
+        if !self.dict.contains_key(name) {
+            match spectra::Oned::new(
+                name,
+                parameter,
+                pdict,
+                Some(axis.low),
+                Some(axis.high),
+                Some(axis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict.insert(sname, Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(msg) => {
+                    return SpectrumReply::Error(msg);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // Make a multi incremented 1d spectrum (gamma-1d)
+
+    fn make_multi1d(
+        &mut self,
+        name: &str,
+        params: &Vec<String>,
+        axis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        if !self.dict.contains_key(name) {
+            match spectra::Multi1d::new(
+                name,
+                params.clone(),
+                pdict,
+                Some(axis.low),
+                Some(axis.high),
+                Some(axis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict
+                        .insert(String::from(name), Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(msg) => {
+                    return SpectrumReply::Error(msg);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // make multi incremented 2-d (gamma2) spectrum:
+
+    fn make_multi2d(
+        &mut self,
+        name: &str,
+        params: &Vec<String>,
+        xaxis: &AxisSpecification,
+        yaxis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        if !self.dict.contains_key(name) {
+            match spectra::Multi2d::new(
+                name,
+                params.clone(),
+                pdict,
+                Some(xaxis.low),
+                Some(xaxis.high),
+                Some(xaxis.bins),
+                Some(yaxis.low),
+                Some(yaxis.high),
+                Some(yaxis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict
+                        .insert(String::from(name), Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(msg) => {
+                    return SpectrumReply::Error(msg);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // make a particle gamma spectrum
+
+    fn make_pgamma(
+        &mut self,
+        name: &str,
+        xparams: &Vec<String>,
+        yparams: &Vec<String>,
+        xaxis: &AxisSpecification,
+        yaxis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        if !self.dict.contains_key(name) {
+            match spectra::PGamma::new(
+                name,
+                xparams,
+                yparams,
+                pdict,
+                Some(xaxis.low),
+                Some(xaxis.high),
+                Some(xaxis.bins),
+                Some(yaxis.low),
+                Some(yaxis.high),
+                Some(yaxis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict
+                        .insert(String::from(name), Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(str) => {
+                    return SpectrumReply::Error(str);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // Make a summary spectrum
+
+    fn make_summary(
+        &mut self,
+        name: &str,
+        params: &Vec<String>,
+        xaxis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        if !self.dict.contains_key(name) {
+            match spectra::Summary::new(
+                name,
+                params.clone(),
+                pdict,
+                Some(xaxis.low),
+                Some(xaxis.high),
+                Some(xaxis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict
+                        .insert(String::from(name), Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(msg) => {
+                    return SpectrumReply::Error(msg);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // Make 2-d spectrum.
+
+    fn make_2d(
+        &mut self,
+        name: &str,
+        xparam: &str,
+        yparam: &str,
+        xaxis: &AxisSpecification,
+        yaxis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        if !self.dict.contains_key(name) {
+            match spectra::Twod::new(
+                name,
+                xparam,
+                yparam,
+                pdict,
+                Some(xaxis.low),
+                Some(xaxis.high),
+                Some(xaxis.bins),
+                Some(yaxis.low),
+                Some(yaxis.high),
+                Some(yaxis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict
+                        .insert(String::from(name), Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(msg) => {
+                    return SpectrumReply::Error(msg);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // Make a 2d sum spectrum.
+
+    fn make_2dsum(
+        &mut self,
+        name: &str,
+        xparams: &Vec<String>,
+        yparams: &Vec<String>,
+        xaxis: &AxisSpecification,
+        yaxis: &AxisSpecification,
+        pdict: &parameters::ParameterDictionary,
+    ) -> SpectrumReply {
+        if !self.dict.contains_key(name) {
+            if xparams.len() != yparams.len() {
+                return SpectrumReply::Error(String::from(
+                    "Number of xparams must be the same as number of y params",
+                ));
+            }
+            let mut params = spectra::XYParameters::new();
+            for (i, x) in xparams.iter().enumerate() {
+                let p: spectra::XYParameter = (x.clone(), yparams[i].clone());
+                params.push(p);
+            }
+            match spectra::TwodSum::new(
+                name,
+                params,
+                pdict,
+                Some(xaxis.low),
+                Some(xaxis.high),
+                Some(xaxis.bins),
+                Some(yaxis.low),
+                Some(yaxis.high),
+                Some(yaxis.bins),
+            ) {
+                Ok(spec) => {
+                    self.dict
+                        .insert(String::from(name), Rc::new(RefCell::new(spec)));
+                    return SpectrumReply::Created;
+                }
+                Err(msg) => {
+                    return SpectrumReply::Error(msg);
+                }
+            }
+        } else {
+            return SpectrumReply::Error(format!("Spectrum {} already exists", name));
+        }
+    }
+    // Delete an existing spectrum.
+
+    fn delete_spectrum(&mut self, name: &str) -> SpectrumReply {
+        if let Some(_) = self.dict.remove(name) {
+            SpectrumReply::Deleted
+        } else {
+            SpectrumReply::Error(format!("Spectrum {} does not exist", name))
+        }
+    }
+    // List spectra and properties.
+
+    fn list_spectra(&self, pattern: &str) -> SpectrumReply {
+        SpectrumReply::Error(String::from("Unimplemnented operation"))
+    }
+    fn gate_spectrum(
+        &self,
+        spectrum: &str,
+        gate: &str,
+        cdict: &conditions::ConditionDictionary,
+    ) -> SpectrumReply {
+        SpectrumReply::Error(String::from("Unimplemnented operation"))
+    }
+    fn ungate_spectrum(&self, spectrum: &str) -> SpectrumReply {
+        SpectrumReply::Error(String::from("Unimplemnented operation"))
+    }
+    fn clear_spectra(&self, pattern: &str) -> SpectrumReply {
+        SpectrumReply::Error(String::from("Unimplemnented operation"))
+    }
+    fn get_contents(
+        &self,
+        name: &str,
+        xlow: f64,
+        xhigh: f64,
+        ylow: f64,
+        yhigh: f64,
+    ) -> SpectrumReply {
+        SpectrumReply::Error(String::from("Unimplemnented operation"))
+    }
+    fn process_events(&mut self, events: &Vec<parameters::Event>) -> SpectrumReply {
+        SpectrumReply::Error(String::from("Unimplemnented operation"))
+    }
+
+    // Public methods
     /// Construction
 
     pub fn new() -> SpectrumProcessor {
         SpectrumProcessor {
-            dict: SpectrumDictionary::new(),
+            dict: spectra::SpectrumDictionary::new(),
         }
     }
     /// Process requests returning replies:
@@ -148,37 +447,57 @@ impl SpectrumProcessor {
                 name,
                 parameter,
                 axis,
-            } =>
-                self.make_1d(&name, &parameter, &axis, &pdict),
-            SpectrumRequest::CreateMulti1D{ name, params, axis} => 
-                self.make_multi1d(&name, &axis, &params, &pdict),
-            SpectrumRequest::CreateMulti2D{name, params, xaxis, yaxis} =>
-                self.make_multi2d(&name, &params, &xaxis, &yaxis),
-            SpectrumRequest::CreatePGamma{name, xparams, yparams, xaxis, yaxis} =>
-                self.make_pgammma(&name, &xparams, &yparams, &xaxis, &yaxis, &pdict),
-            SpectrumRequest::CreateSummary{name, params, yaxis} => 
-                self.make_summary(&name, &params, &yaxis, &pdict),
-            SpectrumRequest::Create2D {name, xparam, yparam, xaxis, yaxis} =>
-                self.make_2d(&name, &xparam, &yparam, &xaxis, &yaxis, &pdict),
-            SpectrumRequest::Create2DSum {name, xparams, yparams, xaxis, yaxis} =>
-                make_2dsum(&name, &xparams, &yparams, &xaxis, &yaxis, &pdict),
-            SpectrumRequest::Delete(name) =>
-                delete_spectrum(&name),
-            SpectrumRequest::List(pattern) =>
-                list_spectra(&pattern),
-            SpectrumRequest::Gate{spectrum, gate} =>
-                gate_spectrum(&spectrum, &gate, &cdict),
-            SpectrumRequest::Ungate(name) =>
-                ungate_spectrum(&name),
-            SpectrumRequest::Clear(pattern) =>
-                clear_spectra(&pattern),
-            SpectrumRequest::GetContents{name, xlow, xhigh, ylow, yhigh} =>
-                get_contents(&name, xlow, xhigh, ylow, yhigh)
-            SpectrumRequest::Events(events) =>
-                process_events(&events)                
+            } => self.make_1d(&name, &parameter, &axis, &pdict),
+            SpectrumRequest::CreateMulti1D { name, params, axis } => {
+                self.make_multi1d(&name, &params, &axis, &pdict)
             }
-
+            SpectrumRequest::CreateMulti2D {
+                name,
+                params,
+                xaxis,
+                yaxis,
+            } => self.make_multi2d(&name, &params, &xaxis, &yaxis, &pdict),
+            SpectrumRequest::CreatePGamma {
+                name,
+                xparams,
+                yparams,
+                xaxis,
+                yaxis,
+            } => self.make_pgamma(&name, &xparams, &yparams, &xaxis, &yaxis, &pdict),
+            SpectrumRequest::CreateSummary {
+                name,
+                params,
+                yaxis,
+            } => self.make_summary(&name, &params, &yaxis, &pdict),
+            SpectrumRequest::Create2D {
+                name,
+                xparam,
+                yparam,
+                xaxis,
+                yaxis,
+            } => self.make_2d(&name, &xparam, &yparam, &xaxis, &yaxis, &pdict),
+            SpectrumRequest::Create2DSum {
+                name,
+                xparams,
+                yparams,
+                xaxis,
+                yaxis,
+            } => self.make_2dsum(&name, &xparams, &yparams, &xaxis, &yaxis, &pdict),
+            SpectrumRequest::Delete(name) => self.delete_spectrum(&name),
+            SpectrumRequest::List(pattern) => self.list_spectra(&pattern),
+            SpectrumRequest::Gate { spectrum, gate } => {
+                self.gate_spectrum(&spectrum, &gate, &cdict)
+            }
+            SpectrumRequest::Ungate(name) => self.ungate_spectrum(&name),
+            SpectrumRequest::Clear(pattern) => self.clear_spectra(&pattern),
+            SpectrumRequest::GetContents {
+                name,
+                xlow,
+                xhigh,
+                ylow,
+                yhigh,
+            } => self.get_contents(&name, xlow, xhigh, ylow, yhigh),
+            SpectrumRequest::Events(events) => self.process_events(&events),
         }
-        SpectrumReply::Error(String::from("Placeholder method"))
     }
 }
