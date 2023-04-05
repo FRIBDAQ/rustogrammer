e6b4dd5550ed02032fed38ae6625f8097e4e01e7
rfoxmich <fox@frib.msu.edu>
2023-04-05 12:52:13 -0400

    Implement the parameter/create method and test it

--- src/rest/parameter.rs
+++ src/rest/parameter.rs
@@ -17,8 +17,6 @@
 
 //#[macro_use]
 //extern crate rocket;
-
-use rocket::serde::ser::SerializeStruct;
 use rocket::serde::{json::Json, Serialize};
 use rocket::State;
 
@@ -49,6 +47,18 @@ pub struct Parameters {
     status: String,
     detail: Vec<ParameterDefinition>,
 }
+
+/// List the parameters:
+///
+/// The URL is
+///
+///    spectcl/parameter/list[?filter=pattern]
+///
+/// If the filter pattern is supplied it must be a valid glob
+/// pattern used to select the names of the parameters
+/// actually listed.  detail on success is an array of
+/// ParameterDefinition values JSON encoded.
+///
 #[get("/list?<filter>")]
 pub fn list_parameters(filter: Option<String>, state: &State<HistogramState>) -> Json<Parameters> {
     let mut result = Parameters {
@@ -94,6 +104,14 @@ pub struct TreeParameterVersion {
     detail: String,
 }
 
+/// Fetch the tree parameter version number.
+/// The URL is of the form:
+///
+///      spectcl/parameter/version
+///
+/// No query parameters are allowed.  The detail on success
+/// is a version string.
+///
 #[get("/version")]
 pub fn parameter_version() -> Json<TreeParameterVersion> {
     let version = TreeParameterVersion {
@@ -102,4 +120,68 @@ pub fn parameter_version() -> Json<TreeParameterVersion> {
     };
 
     Json(version)
+}
+//-----------------------------------------------------
+// What we need to provide the /create method.
+// We're going to allow low, high and bis all to be
+// optional..only requiring name.
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct CreateResponse {
+    status: String,
+    detail: String,
+}
+
+#[get("/create?<name>&<low>&<high>&<bins>&<units>&<description>")]
+pub fn create_parameter(
+    name: String,
+    low: Option<f64>,
+    high: Option<f64>,
+    bins: Option<u32>,
+    units: Option<String>,
+    description: Option<String>,
+    state: &State<HistogramState>,
+) -> Json<CreateResponse> {
+    let mut response = CreateResponse {
+        status: String::from("Ok"),
+        detail: String::new(),
+    };
+
+    // Both low and high must be supplied, not just one:
+
+    if (low.is_none() && high.is_some()) || (low.is_some() && high.is_none()) {
+        response.status = String::from("invalid request");
+        response.detail = String::from("Either low and high must be provided or neither");
+    } else {
+        // Fish out low/high given that either both are there or none:
+
+        let limits = if low.is_some() {
+            Some((low.unwrap(), high.unwrap()))
+        } else {
+            None
+        };
+
+        // Make the API so we can create and, if needed,
+        // modify the metadata:
+
+        let api = ParameterMessageClient::new(&state.inner().state.lock().unwrap().1);
+        let reply = api.create_parameter(&name);
+        match reply {
+            Ok(_) => {
+                // Attempt to set the metadata:
+
+                let status = api.modify_parameter_metadata(&name, bins, limits, units, description);
+                if let Err(s) = status {
+                    response.status = String::from("Failed set parameter metadata: ");
+                    response.detail = s;
+                }
+            }
+            Err(s) => {
+                response.status = String::from("'treeparameter -create' failed: ");
+                response.detail = s;
+            }
+        }
+    }
+    Json(response)
 }
