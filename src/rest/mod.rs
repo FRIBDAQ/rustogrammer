8a8bb872b9715f08ebd29222186ab28e4195119a
rfoxmich <fox@frib.msu.edu>
2023-04-05 12:52:32 -0400

    Documentation fix-ups and additions

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -1,4 +1,4 @@
-//!  This module contains hadlers for the Rocket web
+//!  This module contains handlers for the Rocket web
 //!  server that implement the REST interface for
 //!  that user interfaces can be built against.
 //!  The interface is as compatible with SpecTcl's
@@ -19,7 +19,15 @@
 //!   file that is then re-exported in this top level module
 //!   Which at most will define common data structures
 //!   that can be used across the modules.
-//!    
+//!  
+//!  In general the result of a request is a JSON encoded
+//!  struct with two fields:
+//!  
+//! *  status - which on success is the value "OK" and an
+//! error string if the request failed and that failure is caught
+//! by the handler.
+//! *  detail - whose contents vary depending on the requrest.
+//!
 
 // Re exports:
 
