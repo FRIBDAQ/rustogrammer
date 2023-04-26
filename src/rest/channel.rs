9741f94366ff675362867ca33f4380251531262f
rfoxmich <fox@frib.msu.edu>
2023-04-26 08:51:36 -0400

    Documented what these handlers would do if they were not stubs

--- src/rest/channel.rs
+++ src/rest/channel.rs
@@ -16,7 +16,16 @@ use rocket::State;
 
 use super::*;
 
-// We don't even bother with query parameters.
+/// We don't even bother with query parameters.
+/// If we implement this the query parameters would be:
+///
+/// * spectrum (mandatory)- name of the spectrum.
+/// * xchannel (mandatory)- xchannel number to set.
+/// * ychannel (optional)- y channel number to set.
+/// only makes sense for 2 d spectra.  Defaults to 0.0
+/// if not supplied.
+/// * value - value to set the selected channel to.
+///
 #[get("/set")]
 pub fn set_chan() -> Json<GenericResponse> {
     Json(GenericResponse::err(
@@ -24,6 +33,21 @@ pub fn set_chan() -> Json<GenericResponse> {
         "This is not SpecTcl",
     ))
 }
+/// If this were implemented, 
+/// the query paramters would be:
+///
+/// *   spectrum (mandatory) - name of the spectrum being queried.
+/// *   xchannel (mandatory) - X channel to get
+/// *   ychannel (optional) - required only for 2d spectra. The
+/// Y channel to get.
+///
+/// The return value on success would then be 
+/// *   status : _OK_
+/// *   detail : the value in that channel.
+///
+/// Note that channels out of range would, unlike SpecTcl likely
+/// fetch the over/underflow value depending.
+///
 #[get("/get")]
 pub fn get_chan() -> Json<GenericResponse> {
     Json(GenericResponse::err(
