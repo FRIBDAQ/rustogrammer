d73b5e696a63367814c626106394d55de9be489a
rfoxmich <fox@frib.msu.edu>
2023-04-28 06:44:16 -0400

    *   Created and started testing allocator for spectrum space in Xamine shared memory. *   Added get_free_slot method to xamine shared memory struct.  Part of being able to allocate slot and storage to bind a spectrum.

--- src/sharedmem/mod.rs
+++ src/sharedmem/mod.rs
@@ -27,6 +27,7 @@ const TITLE_LENGTH: usize = 128;
 /// Types of spectra:
 
 #[repr(C)]
+#[derive(PartialEq, Copy, Clone)]
 enum SpectrumTypes {
     undefined = 0,
     twodlong = 5,
@@ -84,18 +85,115 @@ struct XamineSharedMemory {
     dsp_statistics: [Statistics; XAMINE_MAXSPEC],
 }
 
+/// This struct manages storage in some external extent
+/// with a certain fixed size.   It will be used to allocate
+/// and free spectrum storage within the shared memory of
+/// SpecTcl.
+/// Each extent is a tuple of base offset and size.
+///
+type Extent = (usize, usize);
+struct StorageAllocator {
+    free_extents: Vec<Extent>,
+    allocated_extents: Vec<Extent>,
+}
+impl StorageAllocator {
+    // Defragment the free_extents array.
+    // We assume that the number of extents is not that big so
+    // in order to fully defragment, we
+    // 1. sort the free extents by offsets.
+    // 2. build a new free extents vector combining adjacent allocations.
+    //
+    fn defragment(&mut self) {
+        // Sort the
+        self.free_extents.sort_by_key(|e| e.0); // Sort by extent base:
+        let mut result = vec![self.free_extents[0]];
+
+        // This loop works correctly because we know that
+        // free_extents is sorted by offset:
+
+        for item in self.free_extents.iter().skip(1) {
+            // item is contiguous with the last extent, just
+            // modify the extent
+
+            if item.0 == (result[result.len() - 1].0 + result[result.len()].1) {
+                let index = result.len();
+                result[index].1 += item.1
+            } else {
+                // make a new extent not contiguous with the last one.
+
+                result.push(*item);
+            }
+        }
+
+        self.free_extents = result;
+    }
+
+    /// Create a  manager for an arena that's n units bit:
+    ///
+    fn new(n: usize) -> StorageAllocator {
+        StorageAllocator {
+            free_extents: vec![(0, n)],
+            allocated_extents: vec![],
+        }
+    }
+    /// Allocate - our allocator is stupid in that rather than looking
+    /// for the best fit it just provides the first fit.
+    ///
+    fn allocate(&mut self, n: usize) -> Option<usize> {
+        for (i, extent) in self.free_extents.iter().enumerate() {
+            if extent.1 >= n {
+                let result = extent.0;
+                let remainder = extent.1 - n;
+
+                // If the extent was fully used,
+                // remove it else recompute the base/size in place:
+
+                if remainder > 0 {
+                    self.free_extents[i] = (self.free_extents[i].0 + n, self.free_extents[i].1 - n);
+                } else {
+                    self.free_extents.remove(i);
+                }
+                // Mark allocated.
+
+                self.allocated_extents.push((result, n));
+                return Some(result);
+            }
+        }
+        None
+    }
+    /// Free storage.  Error if the freed storage is not in the
+    /// allocated_extents list.
+    /// We defragment on each free - which is a bit excessive but
+    /// allocation/deallocation is believed to be realtively 'slow'.
+    ///
+    fn free(&mut self, offset: usize, size: usize) -> Result<(), String> {
+        let allocation: Extent = (offset, size);
+        for (i, extent) in self.allocated_extents.iter().enumerate() {
+            if (extent.0 == allocation.0) && (extent.1 == allocation.1) {
+                self.allocated_extents.remove(i);
+                self.free_extents.push(allocation);
+                self.defragment();
+                return Ok(());
+            }
+        }
+        Err(String::from("Attempted free of unallocated extent"))
+    }
+}
+
 ///  This struct, and its implementation, define an Xamine
 /// compatible memory and map.
 /// The implementation supports the operations we need on
 /// the memory region.
 pub struct SharedMemory {
-    bindings : [String; XAMINE_MAXSPEC],
-    backing_store : tempfile::NamedTempFile,
-    map : memmap::MmapMut,
-    spec_size: usize
+    bindings: [String; XAMINE_MAXSPEC],
+    backing_store: tempfile::NamedTempFile,
+    map: memmap::MmapMut,
+    spec_size: usize,
 }
 
 impl SharedMemory {
+    /// Make an initialized bindings array that can be
+    /// used to initialize the bindings member.
     fn init_bindings() -> [String; XAMINE_MAXSPEC] {
         // Maybe there's a better way to do this but I'm not sure
         // what it is:
@@ -106,8 +204,17 @@ impl SharedMemory {
         }
         b.try_into().expect("Failed to unwrap the bindings vector")
     }
-    fn total_memory_size(spec_storage_size : usize) -> usize{
+    /// Compute total memory size requirements.
+    fn total_memory_size(spec_storage_size: usize) -> usize {
         spec_storage_size + mem::size_of::<XamineSharedMemory>()
+    }
+    /// Get base of spectrumstorage.
+    fn spectrum_pointer(&mut self) -> *mut u8 {
+        unsafe {
+            self.map
+                .as_mut_ptr()
+                .offset(mem::size_of::<XamineSharedMemory>() as isize)
+        }
     }
     /// Create a new Xamine shared memory region and initialize
     /// it so that there are no spectra in it.
@@ -118,11 +225,11 @@ impl SharedMemory {
     ///  *   specsize - Number of bytes of spectrum storage.
     ///
     /// If there is an error in creating the memory map
-    /// (one reason we know of in Linux is a size limit on 
+    /// (one reason we know of in Linux is a size limit on
     /// shared memory segments), a textual description of the error
     /// is returned as Err, otherwise Ok returns the struct.
     ///
-    pub fn new(specsize : usize) -> Result<SharedMemory, String> {
+    pub fn new(specsize: usize) -> Result<SharedMemory, String> {
         let total_size = Self::total_memory_size(specsize);
         let home_dir = if let Some(s) = dirs::home_dir() {
             s
@@ -131,18 +238,25 @@ impl SharedMemory {
         };
         let file = match tempfile::NamedTempFile::new_in(home_dir) {
             Ok(f) => f,
-            Err(e) => {return Err(format!("Failed to create temp file: {}", e.to_string()));}
+            Err(e) => {
+                return Err(format!("Failed to create temp file: {}", e.to_string()));
+            }
         };
         // Now we need to set the file length:
 
         if let Err(e) = file.as_file().set_len(total_size as u64) {
-            return Err(format!("Failed to set the length of the backing store file: {}", e.to_string()));
+            return Err(format!(
+                "Failed to set the length of the backing store file: {}",
+                e.to_string()
+            ));
         }
         // Finally we can make the memory map:
 
-        let mut map =  match unsafe {memmap::MmapMut::map_mut(file.as_file())} {
+        let mut map = match unsafe { memmap::MmapMut::map_mut(file.as_file()) } {
             Ok(m) => m,
-            Err(e) => {return Err(format!("Failed to map file: {}", e.to_string()));}
+            Err(e) => {
+                return Err(format!("Failed to map file: {}", e.to_string()));
+            }
         };
         // Initialize the map by setting it to zeros.
 
@@ -158,8 +272,32 @@ impl SharedMemory {
         Ok(SharedMemory {
             bindings: Self::init_bindings(),
             backing_store: file,
-            map : map,
-            spec_size : specsize
+            map: map,
+            spec_size: specsize,
         })
+    }
+    /// Get a free slot number.
+
+    pub fn get_free_slot(&self) -> Option<usize> {
+        let header = self.map.as_ptr() as *mut XamineSharedMemory;
+        for i in 0..XAMINE_MAXSPEC {
+            if unsafe { (*header).dsp_types[i] } == SpectrumTypes::undefined {
+                return Some(i);
+            }
+        }
+        return None; // No free slots.
+    }
+}
+
+//// Tests for the allocator:
+
+#[cfg(test)]
+mod allocator_tests {
+    use super::*;
+    #[test]
+    fn alloc_1() {
+        let mut arena = StorageAllocator::new(100);
+        let result = arena.allocate(10).expect("Allocatio of 10 failed"); // should work.
+        assert_eq!(0, result);
     }
 }
