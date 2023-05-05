24ebe938a080cf133fb8cbdd4f1d788479c23728
rfoxmich <fox@frib.msu.edu>
2023-05-05 09:59:16 -0400

    Added /spectcl/shutdown URL

--- src/rest/exit.rs
+++ src/rest/exit.rs
@@ -11,10 +11,31 @@ use rocket::serde::{json::Json, Serialize};
 use rocket::Shutdown;
 
 use super::*; // For generic response.
+use crate::histogramer;
+use crate::sharedmem::binder::BindingApi;
 
 /// This performs the shutdown:
 ///
-#[get["/exit"]]
+#[get["/"]]
 pub fn shutdown(shutdown: Shutdown, state: &State<HistogramState>) -> Json<GenericResponse> {
-    Json(GenericResponse::ok(""))
+    // Shutdown the processor:
+
+    let prc_api = state.inner().processing.lock().unwrap();
+    prc_api
+        .stop_thread()
+        .expect("Failed to stop processing thread!");
+
+    // Shutdown the shared memory program.
+
+    let bind_api = BindingApi::new(&state.inner().binder.lock().unwrap().0);
+    bind_api.exit().expect("Unable to stop the bind thread");
+
+    // Shutdown the histogrammer
+
+    let hg = state.inner().state.lock().unwrap();
+    histogramer::stop_server(&hg.1);
+
+    //  Tell rocket to shutdown when processing of all requests is complete:
+    shutdown.notify();
+    Json(GenericResponse::ok("")) // Client may not get this.
 }
