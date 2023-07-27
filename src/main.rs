06ffb79f2223ce177d6f2ce4af19803868ee5ec3
Ron Fox <fox@frib.msu.edu>
2023-07-27 07:54:34 -0400

    Issue #50 - Implement trace functionality/ReST interface.
    Import the trace implementation module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-//! Todo:
-//!  *  Get the port number from the commandl ine
-//!  *  Allow that port number to be 'managed' which means get it
-//!from the port manager.
-//!  *  Get the shared memory size from the command line.
-//!
 mod conditions;
 mod histogramer;
 mod messaging;
@@ -14,6 +8,7 @@ mod ring_items;
 mod sharedmem;
 mod spectclio;
 mod spectra;
+mod trace;
 
 use clap::Parser;
 use portman_client;
