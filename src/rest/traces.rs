4ccc0dbeb773749e09b533f1886dfa65b683c2fa
Ron Fox <fox@frib.msu.edu>
2023-08-01 08:52:51 -0400

    Issue #50 - Implement trace functionality/ReST interface.
    *   Add test scaffolding for the trace family of ReST handlers.
    *   Test for the establish ReST method.
    *   Test the done ReST method.

--- src/rest/traces.rs
+++ src/rest/traces.rs
@@ -185,3 +185,224 @@ pub fn fetch_traces(
 
     Json(result)
 }
+
+// Tests for the ReST interface that, by their nature are integration tests
+// for the entire tracing subsystem.
+
+#[cfg(test)]
+mod trace_rest_tests {
+    use super::*;
+    use crate::histogramer;
+    use crate::messaging;
+    use crate::trace;
+
+    use rocket;
+    use rocket::local::blocking::Client;
+    use rocket::Build;
+    use rocket::Rocket;
+
+    use std::fs;
+    use std::path::Path;
+    use std::sync::mpsc;
+    use std::sync::Mutex;
+    use std::thread;
+    use std::time;
+    fn setup() -> Rocket<Build> {
+        let tracedb = trace::SharedTraceStore::new();
+        let (_, hg_sender) = histogramer::start_server(tracedb.clone());
+
+        let (binder_req, _jh) = binder::start_server(&hg_sender, 8 * 1024 * 1024, &tracedb);
+
+        // Construct the state:
+
+        let state = HistogramState {
+            histogramer: Mutex::new(hg_sender.clone()),
+            binder: Mutex::new(binder_req),
+            processing: Mutex::new(processing::ProcessingApi::new(&hg_sender)),
+            portman_client: None,
+            mirror_exit: Arc::new(Mutex::new(mpsc::channel::<bool>().0)),
+            mirror_port: 0,
+        };
+
+        // Note we have two domains here because of the SpecTcl
+        // divsion between tree parameters and raw parameters.
+
+        rocket::build()
+            .manage(state)
+            .manage(tracedb.clone())
+            .mount("/", routes![establish_trace, trace_done, fetch_traces])
+    }
+    fn getstate(
+        r: &Rocket<Build>,
+    ) -> (
+        mpsc::Sender<messaging::Request>,
+        processing::ProcessingApi,
+        binder::BindingApi,
+        trace::SharedTraceStore,
+    ) {
+        let chan = r
+            .state::<HistogramState>()
+            .expect("Valid state")
+            .histogramer
+            .lock()
+            .unwrap()
+            .clone();
+        let papi = r
+            .state::<HistogramState>()
+            .expect("Valid State")
+            .processing
+            .lock()
+            .unwrap()
+            .clone();
+        let binder_api = binder::BindingApi::new(
+            &r.state::<HistogramState>()
+                .expect("Valid State")
+                .binder
+                .lock()
+                .unwrap(),
+        );
+        let tracedb = r
+            .state::<trace::SharedTraceStore>()
+            .expect("Valid tracedb")
+            .clone();
+        (chan, papi, binder_api, tracedb)
+    }
+    fn teardown(
+        c: mpsc::Sender<messaging::Request>,
+        p: &processing::ProcessingApi,
+        b: &binder::BindingApi,
+    ) {
+        let backing_file = b.exit().expect("Forcing binding thread to exit");
+        thread::sleep(time::Duration::from_millis(100));
+        let _ = fs::remove_file(Path::new(&backing_file)); // faliure is ok.
+        histogramer::stop_server(&c);
+        p.stop_thread().expect("Stopping processing thread");
+    }
+    #[test]
+    fn establish_1() {
+        // establish a single client - our token will be 0.
+
+        let rocket = setup();
+        let (msg_chan, papi, binder_api, _tracdb) = getstate(&rocket);
+
+        let client = Client::untracked(rocket).expect("making rocket client");
+        let request = client.get("/establish?retention=10");
+        let response = request
+            .dispatch()
+            .into_json::<EstablishResponse>()
+            .expect("Parsing JSON");
+
+        assert_eq!("OK", response.status);
+        assert_eq!(0, response.detail);
+
+        teardown(msg_chan, &papi, &binder_api);
+    }
+    #[test]
+    fn establish_2() {
+        // Establishing two clients should give different tokens:
+
+        let rocket = setup();
+        let (msg_chan, papi, binder_api, _tracdb) = getstate(&rocket);
+
+        let client = Client::untracked(rocket).expect("making rocket client");
+        let request = client.get("/establish?retention=10");
+        let response = request
+            .dispatch()
+            .into_json::<EstablishResponse>()
+            .expect("Parsing JSON");
+
+        assert_eq!("OK", response.status);
+        assert_eq!(0, response.detail);
+        let tok1 = response.detail;
+
+        let request = client.get("/establish?retention=10");
+        let response = request
+            .dispatch()
+            .into_json::<EstablishResponse>()
+            .expect("Parsing JSON");
+        assert_eq!("OK", response.status);
+        assert!(tok1 != response.detail); // This is actually the only required test.
+        assert_eq!(1, response.detail); // White box knowing how they're supposed to be allocated
+
+        teardown(msg_chan, &papi, &binder_api);
+    }
+    #[test]
+    fn done_1() {
+        // Done on a token we don't have is an error:
+
+        let rocket = setup();
+        let (msg_chan, papi, binder_api, _tracdb) = getstate(&rocket);
+
+        let client = Client::untracked(rocket).expect("Making client");
+        let request = client.get("/done?token=0");
+        let reply = request
+            .dispatch()
+            .into_json::<GenericResponse>()
+            .expect("parsing JSON");
+
+        assert!("OK" != reply.status);
+
+        teardown(msg_chan, &papi, &binder_api);
+    }
+    #[test]
+    fn done_2() {
+        // can successfully be done with an allocated token.
+
+        let rocket = setup();
+        let (msg_chan, papi, binder_api, _tracedb) = getstate(&rocket);
+
+        let client = Client::untracked(rocket).expect("Creating client");
+        let establish = client.get("/establish?retention=10");
+        let est_response = establish
+            .dispatch()
+            .into_json::<EstablishResponse>()
+            .expect("parsing JSON");
+
+        assert_eq!("OK", est_response.status);
+        let token = est_response.detail;
+
+        let free_uri = format!("/done?token={}", token);
+        let free = client.get(&free_uri);
+        let free_response = free
+            .dispatch()
+            .into_json::<GenericResponse>()
+            .expect("Parsing JSON");
+        assert_eq!("OK", free_response.status);
+
+        teardown(msg_chan, &papi, &binder_api);
+    }
+    #[test]
+    fn done_3() {
+        // Can't be done with the same token twice.
+
+        let rocket = setup();
+        let (msg_chan, papi, binder_api, _tracedb) = getstate(&rocket);
+
+        let client = Client::untracked(rocket).expect("Creating client");
+        let establish = client.get("/establish?retention=10");
+        let est_response = establish
+            .dispatch()
+            .into_json::<EstablishResponse>()
+            .expect("parsing JSON");
+
+        assert_eq!("OK", est_response.status);
+        let token = est_response.detail;
+
+        let free_req = format!("/done?token={}", token);
+        let free = client.get(&free_req);
+        let free_response = free
+            .dispatch()
+            .into_json::<GenericResponse>()
+            .expect("Parsing JSON");
+        assert_eq!("OK", free_response.status);
+
+        let free = client.get(&free_req);
+        let free_response = free
+            .dispatch()
+            .into_json::<GenericResponse>()
+            .expect("Parsing JSON");
+        assert!("OK" != free_response.status);
+
+        teardown(msg_chan, &papi, &binder_api);
+    }
+}
