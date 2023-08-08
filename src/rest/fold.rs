e8bc2ae282c24e06e867b9307be83fc003f1edab
Ron Fox <fox@frib.msu.edu>
2023-08-08 08:31:38 -0400

    Issue #86 - Centralize setup/teardown code common to REST tests
    Refactor fold tests in terms of rest_common test code.

--- src/rest/fold.rs
+++ src/rest/fold.rs
@@ -81,64 +81,29 @@ pub fn remove() -> Json<GenericResponse> {
 #[cfg(test)]
 mod fold_tests {
     use super::*;
-    use crate::histogramer;
     use crate::messaging;
     use crate::processing;
-    use crate::sharedmem::binder;
-    use crate::trace;
+    use crate::test::rest_common;
 
     use rocket;
     use rocket::local::blocking::Client;
     use rocket::Build;
     use rocket::Rocket;
 
-    use std::sync::{mpsc, Arc, Mutex};
+    use std::sync::mpsc;
     // note these are all unimplemented URLS so...
 
     fn setup() -> Rocket<Build> {
-        let tracedb = trace::SharedTraceStore::new();
-        let (_, hg_sender) = histogramer::start_server(tracedb.clone());
-        let (binder_req, _rx): (
-            mpsc::Sender<binder::Request>,
-            mpsc::Receiver<binder::Request>,
-        ) = mpsc::channel();
-
-        // Construct the state:
-
-        let state = MirrorState {
-            mirror_exit: Arc::new(Mutex::new(mpsc::channel::<bool>().0)),
-            mirror_port: 0,
-        };
-
-        rocket::build()
-            .manage(state)
-            .manage(Mutex::new(hg_sender.clone()))
-            .manage(Mutex::new(binder_req))
-            .manage(Mutex::new(processing::ProcessingApi::new(&hg_sender)))
-            .manage(tracedb.clone())
+        rest_common::setup()
             .mount("/", routes![crate::fold::apply, list, remove])
     }
     fn teardown(c: mpsc::Sender<messaging::Request>, p: &processing::ProcessingApi) {
-        histogramer::stop_server(&c);
-        p.stop_thread().expect("Stopping processing thread");
+        rest_common::teardown(c, p);
     }
     fn get_state(
         r: &Rocket<Build>,
     ) -> (mpsc::Sender<messaging::Request>, processing::ProcessingApi) {
-        let chan = r
-            .state::<SharedHistogramChannel>()
-            .expect("Valid state")
-            .lock()
-            .unwrap()
-            .clone();
-        let papi = r
-            .state::<SharedProcessingApi>()
-            .expect("Valid State")
-            .lock()
-            .unwrap()
-            .clone();
-
-        (chan, papi)
+        rest_common::get_state(r)
     }
     // Note none of thes URIS are implemented so the tests are
     // simple and don't need any other setup.  These tests are
