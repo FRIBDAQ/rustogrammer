9d182a18b264ab36def2746398363223952b7110
Ron Fox <fox@frib.msu.edu>
2023-08-24 11:27:52 -0400

    Issue #71 - Implement spectrum projections
    Tested contents of Y projection of a pgamma spectrum

--- src/projections/mod.rs
+++ src/projections/mod.rs
@@ -1172,28 +1172,64 @@ mod make_spectrum_tests {
 
         let mut data = vec![];
         for i in 0..props.xaxis.clone().unwrap().bins {
-            data.push((i+10) as f64);
+            data.push((i + 10) as f64);
         }
 
         let sapi = spectrum_messages::SpectrumMessageClient::new(&ch);
-        assert!(
-            make_projection_spectrum(&sapi, "test1", &props, ProjectionDirection::X, data.clone())
-                .is_ok()
-        );
+        assert!(make_projection_spectrum(
+            &sapi,
+            "test1",
+            &props,
+            ProjectionDirection::X,
+            data.clone()
+        )
+        .is_ok());
 
-        let contents = sapi.get_contents("test1", -2048.0, 2048.0, -2048.0, 2048.0).expect("Getting spectrum contents");
+        let contents = sapi
+            .get_contents("test1", -2048.0, 2048.0, -2048.0, 2048.0)
+            .expect("Getting spectrum contents");
         // No zeroes so
         assert_eq!(data.len(), contents.len());
 
-        for (i,x) in data.iter().enumerate() {
+        for (i, x) in data.iter().enumerate() {
             assert_eq!(*x, contents[i].value, "Data mismatch at index {}", i)
         }
 
         teardown(ch, jh);
-
     }
     #[test]
     fn pgamm_5() {
         // y has correct data
+
+        let (ch, jh) = setup();
+        let props = make_pgamma_properties(&ch);
+        // Some data:
+
+        let mut data = vec![];
+        for i in 0..props.yaxis.clone().unwrap().bins {
+            data.push((i + 10) as f64);
+        }
+
+        let sapi = spectrum_messages::SpectrumMessageClient::new(&ch);
+        assert!(make_projection_spectrum(
+            &sapi,
+            "test1",
+            &props,
+            ProjectionDirection::Y,
+            data.clone()
+        )
+        .is_ok());
+
+        let contents = sapi
+            .get_contents("test1", -2048.0, 2048.0, -2048.0, 2048.0)
+            .expect("Getting spectrum contents");
+        // No zeroes so
+        assert_eq!(data.len(), contents.len());
+
+        for (i, x) in data.iter().enumerate() {
+            assert_eq!(*x, contents[i].value, "Data mismatch at index {}", i)
+        }
+
+        teardown(ch, jh);
     }
 }
