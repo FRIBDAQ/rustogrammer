5ba5412f70b1a7cc8388fb52773525b4401d5569
Ron Fox <fox@frib.msu.edu>
2023-12-15 07:19:49 -0500

    Issue # 136 GUI for SpecTcl/Rustogramer
    Describe the GUI shape for projection editors.

--- restclients/Python/editorProjection.py
+++ restclients/Python/editorProjection.py
@@ -1,4 +1,23 @@
-'''  This module will provide a projection spectrum editor when implemented.
+'''  
+    This module will provides a projection definition editor.
+    Projection spectra take an existing 2d spectrum and create a
+    projection of the spectrum onto a specific axis.  Optionally,
+    the projection can be:
+    *  A snapthot, in which case it will not be incremented in future events.
+    *  Within a named contour in which case the resulting spectrum will only be
+       composed of counts within the contour on the parent spectrum and, if not a
+       snapshot only increment when the contour gate is true.
+    Therefore the editor will look something like:
+
+    +-----------------------------------------------+
+    |  Name [ Line edit                           ] |
+    |    Project:            [ ] snapshot           |
+    | +-------------------+  [ ] in contour         |
+    | |  2d spectrum list |  +--------------------+ |
+    | +-------------------+  |  contour list      | |
+    |                        +-------------------|  |
+    |                [  Create/replace ]            |
+    +-----------------------------------------------+
 '''
 
 from PyQt5.QtWidgets import QLabel
