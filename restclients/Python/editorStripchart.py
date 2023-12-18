a81eebeaf99bbb27366e5564e0275a2c858ef8e5
Ron Fox <fox@frib.msu.edu>
2023-12-18 08:10:42 -0500

    Issue # 136 GUI for SpecTcl/Rustogramer
    Did layout of Strip Chart editor.

--- restclients/Python/editorStripchart.py
+++ restclients/Python/editorStripchart.py
@@ -1,9 +1,84 @@
-'''  This module will provide a 2d spectrum editor when implemented.
+'''  This module provides a strip chart spectrum editor.   Strip chart
+    spectra are spectra with two parameters.  An xparameter is usually
+    time related.  The Y parameter is summed over the time the x parameter
+    dwells in a bin.  The result is a 1-d spectrum that looks a lot like
+    a strip chart.  If the x parameter goes off the end of the x axis,
+    the x axis is redefined to includde the X parameter (and some slop) 
+    and the data are scrolled - just like a real strip chart recorder.
+
+    At the time this module is being written, only SpecTcl supports
+    strip chart spectra.
+
+    The GUI looks like this:
+
+    +-------------------------------------------------+
+    |  Name: [   line editor for spectrum name]       |
+    |    [X parameter chooser.]  [parameter chooser]  |
+    |             [x axis input]                      |
+    |            [ create/replace ]                   |
+    +-------------------------------------------------+
+
+    Signals:
+ *   commit - the Create/Replace button was clicked.
+ 
+ 
+   Attributes:
+ *    name  - spectrum name.
+ *    xparam - Xparameter name.
+ *    yparam - Yparameter name.
+ *    low, high, bins - axis specification.
+
 '''
 
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import (
+    QWidget, QLabel, QLineEdit, QPushButton, 
+    QGridLayout, QVBoxLayout, QHBoxLayout,
+    QApplication, QMainWindow
+)
+from PyQt5.QtCore import pyqtSignal, Qt
+from ParameterChooser import LabeledParameterChooser, update_model
 
-class StripChartEditor(QLabel):
+from axisdef import AxisInput
+from rustogramer_client import rustogramer as Client
+
+
+class StripChartEditor(QWidget):
     def __init__(self, *args):
         super().__init__(*args)
-        self.setText('Not Implemented yet')
+        layout = QGridLayout()
+
+        layout.addWidget(QLabel('Name:', self), 0,0)
+        self._spectrumName = QLineEdit(self)
+        layout.addWidget(self._spectrumName, 0,1)
+
+        xplayout = QVBoxLayout()
+        xplayout.addWidget(QLabel('Time parameter', self))
+        self._time = LabeledParameterChooser(self)
+        xplayout.addWidget(self._time)
+        layout.addLayout(xplayout, 1,0)
+
+        yplayout = QVBoxLayout()
+        yplayout.addWidget(QLabel('Vertical parameter', self))
+        self._vparam = LabeledParameterChooser(self)
+        yplayout.addWidget(self._vparam)
+        layout.addLayout(yplayout, 1,1)
+
+        self._axis = AxisInput(self)
+        layout.addWidget(self._axis, 2, 0, 1,2, Qt.AlignHCenter)
+
+        self._commit = QPushButton('Create/Replace', self)
+        layout.addWidget(self._commit, 3, 0, 1,2, Qt.AlignCenter)
+
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    c   = QMainWindow()
+    w   = StripChartEditor(c)
+
+    client = Client({'host': 'localhost', 'port': 8000})
+    update_model(client)
+
+    c.setCentralWidget(w)
+    c.show()
+    app.exec()
