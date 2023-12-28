28161007d43bbb0ec99a876524376b7a957f0595
Ron Fox <fox@frib.msu.edu>
2023-12-28 12:20:31 -0500

    Issue # 136 GUI for SpecTcl/Rustogramer Start writing the main program: *   Argument parsing *   client generation. *   top level tabs *   put the correct items in tabs though only SpectrumWidget actually works (the others are just QWidget placeholders).

---
+++ restclients/Python/Gui.py
@@ -0,0 +1,65 @@
+''' Top level GUI (Main program)
+    the following program parameters are supported
+
+    --host - host running rustogramer or SpecTcl - defaults to 'localost'
+    --port - port on which the REST server is listening (defaults to 8000)
+    --service - Defaults to None - service the REST server advertises
+    --service_user - User the service is advertised under defaults to the name of the current user.
+
+'''
+
+from argparse import ArgumentParser
+import os
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QTabWidget, QWidget
+)
+from spectra import SpectrumWidget
+import spectra
+import argparse
+import capabilities
+from rustogramer_client import rustogramer as RestClient
+
+parser = argparse.ArgumentParser(
+    prog='Gui.py',
+    description='User interface GUI for SpecTcl and rustogramer programs',
+    epilog ='If --service and --port are provided, --service overrides.  If --service is not provided, --port is used.'
+)
+parser.add_argument('-H', '--host', 
+    default='localhost', action='store', help='Host on which the histogram program is running'
+)
+parser.add_argument('-p', '--port', 
+    default=8000, action='store', help='Port on which the histogramer REST server is listening for connections defaults to "8000"'
+)
+parser.add_argument('-s', '--service', default=None, action='store', help='Service the REST server advertises defaults to None')
+parser.add_argument('-u', '--service-user', default=os.getlogin(), action='store', help=f'Username the REST server advertises under defaults to "{os.getlogin()}"')
+
+args = parser.parse_args()
+
+client_args = {'host' : args.host, 'port':args.port}
+if args.service is not None:
+    client_args['service'] = args.service
+    client_args['user']    = args.service_user
+
+client = RestClient(client_args)
+spectra.set_client(client)
+capabilities.set_client(client)
+
+
+#  Build the GUI:
+
+app = QApplication(sys.argv)
+main = QMainWindow()
+
+tabs = QTabWidget()
+tabs.addTab(spectra.SpectrumWidget(),'Spectra')
+tabs.addTab(QWidget(), 'Parameters')
+if capabilities.get_program() == capabilities.Program.SpecTcl:
+    tabs.addTab(QWidget(), 'Variables')
+tabs.addTab(QWidget(), 'Gates')
+
+main.setCentralWidget(tabs)
+
+main.show()
+app.exec()
+ 
