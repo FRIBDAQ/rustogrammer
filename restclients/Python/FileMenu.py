db2063f2ef20dd9226aa4fee11e9722d5bde3119
Ron Fox <rfoxkendo@gmail.com>
2024-01-24 11:14:10 -0500

    Issue #136 GUI for SpecTcl/Rustogramer
    Add handler for asking server to save histograms to file.

--- restclients/Python/FileMenu.py
+++ restclients/Python/FileMenu.py
@@ -1,7 +1,12 @@
-from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import (
+    QAction, QDialog, QDialogButtonBox, QVBoxLayout, QHBoxLayout, QRadioButton, QFileDialog
+)
 from PyQt5.QtCore import QObject
 import capabilities
-from spectrumeditor import confirm
+from spectrumeditor import confirm, error
+import SpectrumList
+import os
+from  rustogramer_client import RustogramerException
 
 class FileMenu(QObject):
     ''' 
@@ -30,7 +35,8 @@ class FileMenu(QObject):
         self._save_treevars = QAction('Save Treevariables...', self)
         self._menu.addAction(self._save_treevars)
         
-        self._save_spectra = QAction('Save spectrm contents...', self)
+        self._save_spectra = QAction('Save spectrum contents...', self)
+        self._save_spectra.triggered.connect(self._saveSpectra)
         self._menu.addAction(self._save_spectra)
         
         self._menu.addSeparator()
@@ -68,7 +74,27 @@ class FileMenu(QObject):
     def _saveSpectra(self):
         #  Prompt for spectra to save and the format
         #  and prompt for a file to save them into...
-        pass
+        namePrompter = SpectrumSaveDialog(self._menu)
+        if namePrompter.exec():
+            names = namePrompter.selectedSpectra()
+            format = namePrompter.format()
+            
+            wdir = os.getcwd()
+            if format == 'json':
+                default_ext = 'json'
+            elif format == 'ascii':
+                default_ext = 'spec'
+            elif format == 'binary':
+                default_ext = 'bin'
+            else:
+                default_ext = 'spec'
+            if len(names) > 0:
+                name = QFileDialog.getSaveFileName(self._menu, 'Spectrum File', wdir, default_ext)
+                filename  = self._genfilename(name)
+                try:
+                    self._client.spectrum_write(filename, format, names)
+                except RustogramerException as e:
+                    error(f'Failed to save spectra to {filename} : {e}')
         
     def _exitGui(self):
         #  Make sure the user is certain and if so, exit:
@@ -79,8 +105,81 @@ class FileMenu(QObject):
         if confirm('Are you sure you want to exit the GUI and the histogramer?'):
             self._client.kill_histogramer()
             exit()
+    # utilities
+    
+    def _genfilename(self, dialog_name):
+        # Generate he filename from what comes back from QFileDialog:
+        name = dialog_name[0]
+        
+        # IF this has an extension we're golden - that's the case if splitext[1] has a period.
+        # otherwise we need to glue on the default extension.
+        
+        parts = os.path.splitext(name)
+        print(parts)
+        if '.' in parts[1]:
+            return name
+        else:
+            return name + '.' + dialog_name[1]
             
             
+        
+        
+        
+class SpectrumSaveDialog(QDialog):
+    '''
+    This class provides:
+    *   A standard way to select spectra.
+    *   A selection of formats with which to save the spectra (appropriate to the server)
+    *   Buttons for Ok and Cancel.
+    *  Selectors to get the selected spectra and the format name.
+    '''
+    def __init__(self, *args):
+        super().__init__(*args)
+        layout = QVBoxLayout()
+        
+        self._selection = SpectrumList.SpectrumSelector(capabilities.get_client(), self)
+        layout.addWidget(self._selection)
+        
+        #  Radio boxes for formats:
+        
+        formats = capabilities.get_supported_spectrum_format_strings()
+        radio_layout = QHBoxLayout()
+        
+        self._formats = []
+        for (i, format) in enumerate(formats):
+            button = QRadioButton(format, self)
+            self._formats.append(button)
+            radio_layout.addWidget(button)
+
+    
+        if len(self._formats) > 0:
+            self._formats[0].setChecked(True)   # Default is first format.
+        
+        layout.addLayout(radio_layout)
+        
+        # The buttons at the bottom of the dialog:
+        
+        self._buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        self._buttonBox.accepted.connect(self.accept)
+        self._buttonBox.rejected.connect(self.reject)
+        
+        layout.addWidget(self._buttonBox)
+        
+        self.setLayout(layout)
+        
+
+    
+    def selectedSpectra(self):
+        return self._selection.selected()
+    def format(self):
+        result = None    # In case we have an unknown.
+        
+        for format in self._formats:
+            if format.isChecked():
+                result = format.text()
+                return result
+        
+        return result
         
         
         
