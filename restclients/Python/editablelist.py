d506bf3352d46ad69bdb64a721627e1f8972b2f8
Ron Fox <rfoxkendo@gmail.com>
2024-02-07 11:00:55 -0500

    Issue #136 GUI for SpecTcl/Rustogramer
     Remove the property attempt seems to bother the actual getters.

--- restclients/Python/editablelist.py
+++ restclients/Python/editablelist.py
@@ -35,7 +35,7 @@ from PyQt5.QtWidgets import (
     QAbstractItemView
 )
 
-from PyQt5.QtCore import pyqtSignal, pyqtProperty
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.Qt import *
 
 class EditableList(QWidget):
@@ -117,10 +117,8 @@ class EditableList(QWidget):
             
     #Attribute implementations:
 
-    @pyqtProperty(list)
     def list(self):
         return [self._list.item(x).text() for x in range(self._list.count())]
-    @list.setter
     def setList(self, items):
         self.clear()
         for i in items:
