de167cc525255095576712d0e95893196515ec8f
Ron Fox <rfoxkendo@gmail.com>
2024-03-18 07:56:04 -0400

    Ooops need to tell the GUI that SpecTcl 5.14-000 and later can support Cluster files via REST.

--- restclients/Python/capabilities.py
+++ restclients/Python/capabilities.py
@@ -319,7 +319,7 @@ def has_rest_runlist():
         True if the program can be asked to process a list of runs (cluster file) via REST:
     '''
     
-    return (get_program == Program.SpecTcl) and (combined_version >= _make_combined_version(5,14,0)
+    return (get_program == Program.SpecTcl) and (combined_version >= _make_combined_version(5,14,0))
     
 
 def can_read_raw_events():
